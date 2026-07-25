cb01b76a217337ebeff155251830fd673c4475f4
aek676 <aek676@inlumine.ual.es>
2026-07-26 00:49:02 +0200

    fix(web): fix tsconfig path aliases and add Toaster component

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { Toaster } from 'shadcn-ui/toast';
 import App from './app/app';
 
 const root = ReactDOM.createRoot(
@@ -9,5 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>,
 );
